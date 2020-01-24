9ea81bd727eade080050e10dbf5565749c85febe
aslamm <aslamm.anas29@gmail.com>
2020-01-23 20:37:52 -0500

    Building the Header Component

---
+++ assets/js/hyper/components/Header.js
@@ -0,0 +1,18 @@
+import { h, app } from 'hyperapp';
+
+export default function Header({ state, actions }) {
+	return (
+		<header>
+			<div class="logo">Logos</div>
+			<nav>
+				<a href="#">Menu</a>
+				<a href="#">Party Platters</a>
+				<a href="#">Locations</a>
+				<a href="#">Rewards</a>
+				<a href="#">Reservations</a>
+			</nav>
+		</header>
+	);
+}
+// <Header state={state} actions={actions}/>
+// <Button state={state} actions={actions}/>
