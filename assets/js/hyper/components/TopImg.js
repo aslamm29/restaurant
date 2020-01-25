f4736a167d591dfd44362b2b4a8429b0cc74b710
aslamm <aslamm.anas29@gmail.com>
2020-01-24 20:46:51 -0500

    Styling the TopImg component with new fonts

--- assets/js/hyper/components/TopImg.js
+++ assets/js/hyper/components/TopImg.js
@@ -2,11 +2,13 @@ import { h, app } from 'hyperapp';
 
 export default function TopImg({ state, actions }) {
 	return (
-		<section>
-			<h5>Welcome</h5>
-			<h1>Prime Steak Restaurant</h1>
+		<section class="TopImg">
+			<div class="title">
+				<h5>Welcome</h5>
+				<h1>Prime Steak Restaurant</h1>
+			</div>
 			<div class="contact-info">
-				<div>Book Table Directly</div>
+				<div class="booking">Book Table Directly</div>
 				<h2>(647) - 925 - 7932</h2>
 				<div class="hours">
 					Opening Hours <strong>Mon - Fri: </strong> 9am - 9pm{' '}
