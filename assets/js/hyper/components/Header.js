3c19a2e5102c9398a3e968b71481ecb344b72f7f
aslamm <aslamm.anas29@gmail.com>
2020-01-24 21:06:52 -0500

    Added a CSS container grid to align center

--- assets/js/hyper/components/Header.js
+++ assets/js/hyper/components/Header.js
@@ -3,14 +3,16 @@ import { h, app } from 'hyperapp';
 export default function Header({ state, actions }) {
 	return (
 		<header>
-			<div class="logo">Logos</div>
-			<nav>
-				<a href="#">Menu</a>
-				<a href="#">Party Platters</a>
-				<a href="#">Locations</a>
-				<a href="#">Rewards</a>
-				<a href="#">Reservations</a>
-			</nav>
+			<div class="container">
+				<div class="logo">Logos</div>
+				<nav>
+					<a href="#">Menu</a>
+					<a href="#">Party Platters</a>
+					<a href="#">Locations</a>
+					<a href="#">Rewards</a>
+					<a href="#">Reservations</a>
+				</nav>
+			</div>
 		</header>
 	);
 }
