6a2fcaaf3ed246d984bcdc3f7885194ef94b2083
aslamm <aslamm.anas29@gmail.com>
2020-01-25 23:56:22 -0500

    Styling the SpecialMenu Component

--- assets/js/hyper/components/SpecialMenu.js
+++ assets/js/hyper/components/SpecialMenu.js
@@ -6,12 +6,44 @@ export default function SpecialMenu({ state, actions }) {
 			<div class="container">
 				<h5 class="comp-title">Special Menu</h5>
 				<h2>Delicious Flavour of Autumn</h2>
-				<div class="row">
-					<div class="col-md-4">box</div>
-					<div class="col-md-4">box</div>
-					<div class="col-md-4">box</div>
+				<div class="row boxes">
+					<div class="col-md-4">
+						<div class="box">
+							<div class="box-img">
+								<div class="price-circle">$25</div>
+							</div>
+							<span class="title">Super BBQ Grill No Smoke</span>
+							<p class="details">
+								Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="box">
+							<div class="box-img">
+								<div class="price-circle">$25</div>
+							</div>
+							<span class="title">Super BBQ Grill No Smoke</span>
+							<p class="details">
+								Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables
+							</p>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="box">
+							<div class="box-img">
+								<div class="price-circle">$25</div>
+							</div>
+							<span class="title">Super BBQ Grill No Smoke</span>
+							<p class="details">
+								Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables
+							</p>
+						</div>
+					</div>
 				</div>
-				<a href="#">View Full Menu</a>
+				<a href="#" class="link">
+					View Full Menu
+				</a>
 			</div>
 		</section>
 	);
