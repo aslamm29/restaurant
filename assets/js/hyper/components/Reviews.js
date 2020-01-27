3b7e63a1e4e963221d339f645130454861d51f47
aslamm <aslamm.anas29@gmail.com>
2020-01-27 16:01:58 -0500

    Styling the reviews component

--- assets/js/hyper/components/Reviews.js
+++ assets/js/hyper/components/Reviews.js
@@ -6,29 +6,27 @@ export default function Reviews({ state, actions }) {
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8">
-						<img src="https://s3.amazonaws.com/tinycards/image/8a924d9d7b6647348734a1ca0fcd9a43" />
+						<div class="side-img">
+							<img src="https://s3.amazonaws.com/tinycards/image/8a924d9d7b6647348734a1ca0fcd9a43" />
+						</div>
 					</div>
 					<div class="col-md-4">
 						<h5 class="comp-title">REVIEWS</h5>
 						<h2>The Food Network</h2>
-						<h4>"Best Restaurant in the GTA"</h4>
+						<h4>"Best Restaurant in the GTA!"</h4>
 						<p>
 							Beard meh flexitarian, paleo kitsch meditation dreamcatcher
 							typewriter VHS keffiyeh ennui activated charcoal. Deep v tofu
 							vaporware, yr vinyl man bun ethical raw denim 3 wolf moon
-							affogato. Pinterest fixie bicycle rights tilde quinoa 8-bit
-							authentic church-key 3 wolf moon palo santo pitchfork cred.
-							Humblebrag cliche occupy hexagon pitchfork butcher. DIY meditation
-							schlitz, kinfolk austin skateboard church-key tumeric. Enamel pin
-							tattooed mlkshk, photo booth shabby chic mixtape cronut pug keytar
-							kombucha polaroid dreamcatcher.
+							affogato.
 						</p>
 						<div class="author">
-							<strong>Joe Bastiachi</strong> - winner of the chef masters
+							<strong>Joe Bastiachi</strong> -{' '}
+							<em>winner of the chef masters</em>
 						</div>
 						<div class="arrows">
 							<i class="fas fa-arrow-left"></i>
-							<i class="fas fa-arrow-right"></i>
+							<i class="fas fa-arrow-right ready"></i>
 						</div>
 					</div>
 				</div>
