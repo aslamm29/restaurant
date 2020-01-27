861d57fecd17553af1eeca4347a7aa62662f2bf0
aslamm <aslamm.anas29@gmail.com>
2020-01-27 18:50:27 -0500

    Building the footer component

--- assets/js/hyper/components/App.js
+++ assets/js/hyper/components/App.js
@@ -6,6 +6,7 @@ import SpecialMenu from './SpecialMenu';
 import RandomQuote from './RandomQuote';
 import Reviews from './Reviews';
 import ContactUs from './ContactUs';
+import Footer from './Footer';
 
 export default function App({ state, actions }) {
 	return (
@@ -17,6 +18,7 @@ export default function App({ state, actions }) {
 			<RandomQuote state={state} actions={actions} />
 			<Reviews state={state} actions={actions} />
 			<ContactUs state={state} actions={actions} />
+			<Footer state={state} actions={actions} />
 		</div>
 	);
 }
--- assets/js/hyper/components/Footer.js
+++ assets/js/hyper/components/Footer.js
@@ -1,18 +1,21 @@
 import { h, app } from 'hyperapp';
 
-export default function Header({ state, actions }) {
+export default function Footer({ state, actions }) {
 	return (
-		<header>
+		<footer>
 			<div class="container">
-				<div class="logo">PS</div>
-				<nav>
+				<nav class="menu">
 					<a href="#">Our Story</a>
 					<a href="#">Reviews</a>
 					<a href="#">Special Menu</a>
 					<a href="#">Reservations</a>
 					<a href="#">Contact Us</a>
 				</nav>
+
+				<ul class="social-media"></ul>
+
+				<div class="copyright">© 2018 Copyright</div>
 			</div>
-		</header>
+		</footer>
 	);
 }
