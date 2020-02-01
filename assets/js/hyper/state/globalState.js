7309da74ecbda5007c407fc8cb15cd2531973701
aslamm <aslamm.anas29@gmail.com>
2020-01-31 19:43:52 -0500

    Populating the special Menu data through the state

--- assets/js/hyper/state/globalState.js
+++ assets/js/hyper/state/globalState.js
@@ -4,7 +4,29 @@ var companyInfo = {
 	location: 'Toronto, Ontario'
 };
 
+var specialMenuData = [
+	{
+		title: 'Super BBQ Grill No Smoke',
+		description:
+			'Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables',
+		price: 25
+	},
+	{
+		title: 'Mixed Vegetables',
+		description:
+			'Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables',
+		price: 15
+	},
+	{
+		title: 'All Canadian Mix',
+		description:
+			'Fried Egg, Steak, Baked Potato, French Fries, Side of vegetables',
+		price: 17
+	}
+];
+
 export const globalState = {
 	count: 0,
-	companyInfo
+	companyInfo,
+	specialMenuData
 };
